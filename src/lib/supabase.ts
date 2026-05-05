d91e587ef006d0d366e9c776cad1adf565bc2b6f
keddarilyas776-jpg <keddarilyas776@gmail.com>
2026-05-05 10:43:36 +0100

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
+  || 'https://uskwvutzqprbsrrixmlf.supabase.co'
+
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+  || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVza3d2dXR6cXByYnNycml4bWxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3OTU5OTIsImV4cCI6MjA5MzM3MTk5Mn0.XtG_7hR-Np35lRj1L9cZ_0WFDTzTfuGyDFpkMfObA9Y'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
