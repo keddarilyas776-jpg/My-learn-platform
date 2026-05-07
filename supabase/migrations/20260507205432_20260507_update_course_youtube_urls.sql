2b0044dbd305da1b8bacba2ee981c2b3a6a49558
keddarilyas776-jpg <keddarilyas776@gmail.com>
2026-05-07 22:13:17 +0100

    Added 20260507205432_20260507_update_course_youtube_urls.sql

---
+++ supabase/migrations/20260507205432_20260507_update_course_youtube_urls.sql
@@ -0,0 +1,79 @@
+/*
+  # Update YouTube Playlist URLs for All 24 Courses
+
+  Sets high-quality, Arabic-language YouTube playlist URLs for every course
+  on the platform, matched by course ID. Sources include well-known Arabic
+  educators (Elzero Web School, Codezilla, Ahmed Hassan, Metigator, etc.).
+*/
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PL_jgrjSlhOY-gM9k2VGELWjySJdGo04Kv'
+  WHERE id = 'fbc3c259-02cb-47b7-85a8-1573d7705477'; -- الرسم الإبداعي خطوة بخطوة
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLakxjTzn4UnBK1HOwL9536jKOagcewsgl'
+  WHERE id = '1ec300a7-1de6-4643-aa24-dc88ad291420'; -- فن الخط العربي
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLsKxzTvwxdF752KdHOmTHSP_VUyqXzK_I'
+  WHERE id = '68c77783-2207-4976-b4bb-d79c69f51f46'; -- الرسوم المتحركة 2D
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLq8CELngJ0gSTsTK6O0CnkFtggKGYvVsH'
+  WHERE id = '1c4aac06-f170-4c4e-a9c9-a1c85cbc6b0c'; -- فن التصوير الفوتوغرافي
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PL_1XAjannEMuymvxpTEuphHaqnitsAQe2'
+  WHERE id = 'daa7f9c0-5a48-43cf-8c84-82434d13a4ae'; -- مونتاج الفيديو احترافياً
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLZ21MU350YVF-6zh80_qya01rlLWNHXuo'
+  WHERE id = '68ec9dc5-bda2-4d1f-97fc-0225f90e409e'; -- تعلم العزف على الغيتار
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLJrN2cum_UCQV7rsUtWwA4AfG3HV5Zs9_'
+  WHERE id = '905e0bf1-ddb2-4254-b857-b53280051d48'; -- فن الكتابة الإبداعية
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLhBfC85N-2UFiAs4XpTAAmeYkw17ErYXs'
+  WHERE id = '76a338f5-600b-43db-b296-5d518f94074e'; -- القراءة السريعة
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLjY0VhADk_sdex1nhJy4ia-Ywz3wDWy3p'
+  WHERE id = '4b87929d-a211-4a58-a7ab-194eca5c8ce5'; -- الأدب العربي الكلاسيكي
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLDFaVVnQKqIJEqixR13KbirHMal_wGdJS'
+  WHERE id = '3c032311-bc29-499b-a206-499fd936fb49'; -- اللغة الإنجليزية للمتوسطين
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLoAZTvqMQqh7btwpyZCdV2NPorXkyTeQb'
+  WHERE id = '9319b0fa-e33e-4753-a271-30e2bde76a28'; -- تعلم الفرنسية من الصفر
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PL0VjGd7nigDsCC1OrJgvFLRVTmKWOcusP'
+  WHERE id = '61b5d552-49c2-4561-bbe1-7a1bef6f927d'; -- فن الخطابة والإلقاء
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLqGEgt_8f5gSM6JNsCtixzFfuK5MZlgHi'
+  WHERE id = 'c2586264-9495-49d4-ae5c-738d3cc33aca'; -- إدارة الوقت والإنتاجية
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLDoPjvoNmBAw_t_XWUFbBX-c9MafPk9ji'
+  WHERE id = '778d6327-3d3d-45e9-969d-f28301a080e5'; -- تطوير الويب مع Elzero
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLuXY3ddo_8nzrO74UeZQVZOb5-wIS6krJ'
+  WHERE id = 'ceb193bf-29c2-4c63-b939-2b215de870d8'; -- تعلم Python من الصفر
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PL4n1Qos4Tb6RP_OovpgjoHLkCVaYFy-aj'
+  WHERE id = 'e894460d-cdfc-40e7-bf28-fa08666f6a14'; -- قواعد البيانات SQL
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLoP_aS_FoPQfCHLDULY7wmjz3Qgp5f7il'
+  WHERE id = '1a901896-1e82-419b-af63-6b82102b47ed'; -- أساسيات الشبكات CCNA
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLSiLeKadTQ7mmj3OrpQK5yJi7wPvu59jN'
+  WHERE id = '4f4f74b4-c1ae-4698-a994-ce52d785bdab'; -- أساسيات الذكاء الاصطناعي
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLh2Jy0nKL_j1WZMzITHgUuzaadpSULlMm'
+  WHERE id = '4a2d3d38-45af-4fcd-81c7-ca2ab6b61e2d'; -- أساسيات الأمن السيبراني
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLlzIpiCueEArjVi59d0uw5rpQ8tLLtro0'
+  WHERE id = '134f21d8-39b6-477c-8823-b33c85c64c32'; -- AutoCAD من الصفر للاحتراف
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLxwLGRnEgjN69zrsKlL5KOoXu9Y7n7CaH'
+  WHERE id = '564937a4-2bac-47e5-b7e3-6a8c090beb1d'; -- برمجة PLC باستخدام TIA Portal
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLtRa43yozbyG--Dx304AQg_fmwC6qLazg'
+  WHERE id = 'a46911ec-46d5-4647-a713-c4bd7d136c89'; -- إلكترونيات للمبتدئين
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLLlH75MpFXNcZtAw-AELpQC7DDH2EM__L'
+  WHERE id = 'e4963fc1-de9f-42c5-97ea-ef2711cd3470'; -- التصميم ثلاثي الأبعاد Blender
+
+UPDATE courses SET youtube_url = 'https://www.youtube.com/playlist?list=PLK7KUj7SI53a5EhrX7z2K4rB1tuOwqdc5'
+  WHERE id = '8453eac9-a1bc-4c76-8977-ecd62468b7fd'; -- علم النفس الطبي
